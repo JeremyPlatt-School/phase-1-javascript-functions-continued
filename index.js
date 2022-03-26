e9f48c8631ca4ce3c197eee7e262e343f5bda463
Jeremy <jplatt4427@gmail.com>
2022-03-26 14:39:09 -0700

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(activity){
+    let phrase = "This Saturday, I want to "
+    if (activity == null){
+        phrase = phrase + "roller-skate!"
+    } else {
+        phrase = phrase + activity + "!";
+    }
+    return phrase;
+}
+const mondayWork = function(activity){
+    let phrase = "This Monday, I will "
+    if (activity == null){
+        phrase = phrase + "go to the office.";
+    } else {
+        phrase = phrase + activity + ".";
+    }
+    return phrase;
+}
+function wrapAdjective(highlight){
+    return function(msg){
+        let phrase = "You are "
+    if (msg == null || highlight == null){
+        phrase = phrase + "*special*.";
+    } else {
+        phrase = phrase + highlight + msg + highlight + "!";
+    }
+    return phrase;
+    }
+}
